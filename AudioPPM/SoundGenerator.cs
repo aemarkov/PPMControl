02262b3a3ddd06d53480d88e3d26f5de02f14d3c
Марков Алексей <markovalex95@gmail.com>
2017-09-21 09:41:26 +0300

    Create PPM signal generation using NAudio. PPM is generating, but there is error: All channels are duplicated (1-2-3-4-5-6-1-2-3-4-5-6-SYNC-1-2-...)

---
+++ AudioPPM/SoundGenerator.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace AudioPPM
+{
+    public class SoundGenerator
+    {
+        private IWavePlayer _player;
+
+        public SoundGenerator()
+        {
+            var sineWaveProvider = new PpmProvider(6);
+            
+            _player = new DirectSoundOut();
+            _player.Init(sineWaveProvider);
+
+            
+        }
+
+        public void Play()
+        {
+            _player.Play();
+
+        }
+
+        public void Stop()
+        {
+            _player.Stop();
+        }
+    }
+
+    /// <summary>
+    /// Polarity of the various length part of PPM signal
+    /// </summary>
+    public enum PpmPolarity
+    {
+        HIGH,
+        LOW
+    }
+
+    /// <summary>
+    /// Set of PPM settings (timings and polarity)
+    /// </summary>
+    public struct PpmProfile
+    {
+        /// <summary>
+        /// Duration of the pause
+        /// (part of PPM cycle which duration is constant)
+        /// </summary>
+        public int PauseDuration { get; set; }
+
+        /// <summary>
+        /// Polarity of the various length part of PPM signal
+        /// </summary>
+        public PpmPolarity Polarity { get; set; }
+
+        /// <summary>
+        /// Minimum period duration (mks)
+        /// </summary>
+        public int MinChannelDuration { get; set; }
+
+        /// <summary>
+        /// Maximum period duration (mks)
+        /// </summary>
+        public int MaxChannelDuration { get; set; }
+
+        /// <summary>
+        /// Whole period (all channels + sync idle) (mks)
+        /// </summary>
+        public int Period { get; set; }
+
+        public static PpmProfile Default()
+        {
+            return new PpmProfile()
+            {
+                PauseDuration = 500,
+                Polarity = PpmPolarity.LOW,
+                MinChannelDuration = 1000,
+                MaxChannelDuration = 2000,
+                Period = 20000
+            };
+        }
+    }
+
+
+    public abstract class WaveProvider32 : IWaveProvider
+    {
+        private WaveFormat waveFormat;
+
+        public WaveProvider32()
+            : this(44100, 1)
+        {
+        }
+
+        public WaveProvider32(int sampleRate, int channels)
+        {
+            SetWaveFormat(sampleRate, channels);
+        }
+
+        public void SetWaveFormat(int sampleRate, int channels)
+        {
+            this.waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channels);
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            WaveBuffer waveBuffer = new WaveBuffer(buffer);
+            int samplesRequired = count / 4;
+            int samplesRead = Read(waveBuffer.FloatBuffer, offset / 4, samplesRequired);
+            return samplesRead * 4;
+        }
+
+        public abstract int Read(float[] buffer, int offset, int sampleCount);
+
+        public WaveFormat WaveFormat
+        {
+            get { return waveFormat; }
+        }
+    }
+
+    /// <summary>
+    /// Generate PPM signal
+    /// </summary>
+    public class PpmProvider : WaveProvider32
+    {
+        private WaveFormat _waveFormat;
+        private PpmProfile _ppmProfile;
+
+        // Data buffers. Double buffering is used
+        private byte[][] _buffers;
+        private int _currentBuffer;
+        private int _currentChannel;
+
+        // PPM timing params
+        private int _currentChannelSample;
+        private int _pauseSamples;
+        private int _periodSamples;
+        private int _totalSamples;
+
+        public WaveFormat WaveFormat => _waveFormat;
+
+        /// <summary>
+        /// PPM timings and polarity settings. Read only.
+        /// </summary>
+        public PpmProfile PpmProfile => _ppmProfile;
+
+        public byte Channels { get; private set; }
+
+
+
+        public PpmProvider(byte channels): this(channels, 44100, PpmProfile.Default())
+        {
+        }
+
+
+        public PpmProvider(byte channels, int sampleRate, PpmProfile ppmProfile)
+        {
+            Channels = channels;
+            _ppmProfile = ppmProfile;
+            _waveFormat = new WaveFormat(sampleRate, 1);
+            
+            _buffers = new byte[2][];
+            for (int i = 0; i < 2; i++)
+                _buffers[i] = new byte[] {127, 0,  0, 0, 0, 255};
+
+            _currentBuffer = 0;
+            _currentChannel = 0;
+            _currentChannelSample = 0;
+            _totalSamples = 0;
+
+            TakeNextChannel();
+        }
+
+
+        /// <summary>
+        /// Set new values. They will be applied from new cycle. 
+        /// NOTE: Number of values should be equal to number of channels.
+        /// Otherwise exception will be thrown.
+        /// </summary>
+        /// <param name="values"></param>
+        public void SetValues(byte[] values)
+        {         
+            // 1 - _currentBuffer is buffer swapping
+            Array.Copy(values, _buffers[1 - _currentBuffer], Channels);
+        }
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            int i = offset;
+            while (i < sampleCount)
+            {
+                //byte polarity = _ppmProfile.Polarity == PpmPolarity.LOW ? (byte) 0 : (byte)1;
+                if (_currentChannelSample <= _pauseSamples)
+                    buffer[i] = -1f; //(byte) (1 - polarity);
+                else
+                    buffer[i] = 0; //polarity;
+
+                //Console.WriteLine(buffer[i]);
+
+                _currentChannelSample++;
+                _totalSamples++;
+                if (_currentChannelSample > _periodSamples)
+                    TakeNextChannel();
+
+                i++;
+            }
+
+            return sampleCount;
+        }
+
+
+        /// <summary>
+        /// Get value from next channel and calculate timings
+        /// </summary>
+        private void TakeNextChannel()
+        {
+            _currentChannel++;
+            _currentChannelSample = 0;
+
+            if (_currentChannel == Channels)
+            {
+                //Pause
+                //Duration = Period - sum samples
+
+                _periodSamples = (int)(_ppmProfile.Period / 1000000.0 * _waveFormat.SampleRate - _totalSamples);
+                _pauseSamples = 0;//_periodSamples;
+
+                _totalSamples = 0;
+                _currentChannel = -1;
+
+                //Console.WriteLine("Pause");
+            }
+            else
+            {
+                _pauseSamples = (int)(_ppmProfile.PauseDuration / 1000000.0 * _waveFormat.SampleRate);
+
+                double period = _buffers[_currentBuffer][_currentChannel] / 255.0 *
+                                (_ppmProfile.MaxChannelDuration - _ppmProfile.MinChannelDuration) +
+                                _ppmProfile.MinChannelDuration; 
+
+                _periodSamples =(int) (period / 1000000.0 * _waveFormat.SampleRate);
+
+                //Console.WriteLine($"Channel {_currentChannel}");
+            }
+        }
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioPPM;
+using NAudio.CoreAudioApi;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gen = new SoundGenerator();
+
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            foreach (MMDevice device in enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.All))
+            {
+                Console.WriteLine("{0}, {1}", device.FriendlyName, device.State);
+            }
+
+            gen.Play();
+            Console.ReadKey();
+            gen.Stop();
+        }
+    }
+}
